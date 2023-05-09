40b76fddec49ddc694c69e782c686cc033edf1a5
Patryk Kaczmarek <patryk.kaczmarek@netguru.pl>
2023-05-09 13:14:21 +0200

    Initial commit

---
+++ Floofs/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  Floofs
+//
+
+import UIKit
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let scene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: scene)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+}
---
+++ Floofs/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Floofs
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .red
+    }
+}
