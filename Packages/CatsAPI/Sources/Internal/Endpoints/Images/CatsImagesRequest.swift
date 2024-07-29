a22643e1d9f660ac03b223fadbdcf08e1cb8dcff
Patryk Kaczmarek <patryk.krzysztof.kaczmarek@gmail.com>
2024-07-29 12:19:25 +0200

    Move local libraries to SPM packages

---
+++ Packages/CatsAPI/Sources/Internal/Endpoints/Images/CatsImagesRequest.swift
@@ -0,0 +1,37 @@
+//
+// CatsImagesRequest.swift
+// CatsAPI
+//
+
+import APILayer
+import Foundation
+
+struct CatsImagesRequest: APIRequest {
+    typealias ResponseMapper = CatsImagesResponseMapper
+
+    var path: String {
+        "images/search"
+    }
+
+    var method: APIRequestMethod {
+        .get
+    }
+
+    var apiVersion: APIVersion? {
+        .version1
+    }
+
+    let limit: Int
+
+    let page: Int
+
+    let breedIdentifier: String
+
+    var queryItems: [URLQueryItem]? {
+        [
+            URLQueryItem(name: "limit", value: "\(limit)"),
+            URLQueryItem(name: "page", value: "\(page)"),
+            URLQueryItem(name: "breed_ids", value: breedIdentifier)
+        ]
+    }
+}
---
+++ Packages/DogsAPI/Sources/Public/DogsAPIClient.swift
@@ -0,0 +1,84 @@
+//
+//  DogsAPIClient.swift
+//  DogsAPI
+//
+
+import APILayer
+import Foundation
+
+public final class DogsAPIClient {
+
+    // MARK: - Properties
+
+    private let apiClient: APIClient
+
+    // MARK: - Constructor
+
+    public init(urlSession: URLSession) {
+        apiClient = APIClient(configuration: DogsAPIConfiguration(), session: urlSession)
+    }
+
+    // MARK: - API
+
+    public func fetchAllBreeds(completion: @escaping (Result<[Dog], APIClientError>) -> Void) {
+        apiClient.perform(request: DogsBreedsListRequest()) { [weak self] result in
+            switch result {
+            case .success(let dogs):
+                self?.fetchCoverImages(for: dogs, completion: completion)
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+
+    public func fetchImages(dog: Dog, completion: @escaping (Result<Dog, APIClientError>) -> Void) {
+        guard !dog.images.isComplete else {
+            completion(.success(dog))
+            return
+        }
+        let request = DogsImagesRequest(breed: dog.breed, subbreed: dog.subbreed)
+        apiClient.perform(request: request) { result in
+            switch result {
+            case .success(let urls):
+                var newDog = dog
+                newDog.addImages(urls: urls, isComplete: true)
+                completion(.success(newDog))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
+
+// MARK: - Private
+
+private extension DogsAPIClient {
+    func fetchCoverImages(
+        for dogs: [Dog],
+        completion: @escaping (Result<[Dog], APIClientError>) -> Void
+    ) {
+        let group = DispatchGroup()
+        var dogsWithCoverImages: [Dog] = []
+
+        dogs.forEach { dog in
+            group.enter()
+
+            let request = DogsRandomImageRequest(breed: dog.breed, subbreed: dog.subbreed)
+            apiClient.perform(request: request) { result in
+                switch result {
+                case .success(let url):
+                    var newDog = dog
+                    newDog.addCoverImage(url: url)
+                    dogsWithCoverImages.append(newDog)
+                case .failure:
+                    dogsWithCoverImages.append(dog)
+                }
+                group.leave()
+            }
+        }
+
+        group.notify(queue: .main) {
+            completion(.success(dogsWithCoverImages))
+        }
+    }
+}
